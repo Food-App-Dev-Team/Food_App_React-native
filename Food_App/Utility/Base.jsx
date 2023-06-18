d1489608979edd51fc8adc20197e9c01ad73c75b
bruceg14 <ziyuan.guo@vanderbilt.edu>
2023-06-18 11:41:51 -0500

    Updated Base file

--- Food_App/Utility/Base.jsx
+++ Food_App/Utility/Base.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { TouchableOpacity, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+const Tab = createBottomTabNavigator();
+import Account from "../Pages/Account"
+import Explore from "../Pages/Explore"
+import Friends from "../Pages/Friends"
+import Map from "../Pages/Map"
+import Post from "../Pages/Post"
 
 function Base() {
-    const onPressHandler1 = () => {
-
-    }
-    
-    return (
-        <View>
-            <TouchableOpacity onPress={onPressHandler1}>
-                <View style={{ backgroundColor: 'blue', padding: 10 }}>
-                    <Text style={{ color: 'white', fontSize: 16 }}>Press Me</Text>
-                </View>
-            </TouchableOpacity>
-        </View>
-    )
+    <Tab.Navigator>
+      <Tab.Screen name="Account" component={Account} />
+      <Tab.Screen name="Explore" component={Explore} />
+      <Tab.Screen name="Friends" component={Friends} />
+      <Tab.Screen name="Map" component={Map} />
+      <Tab.Screen name="Post" component={Post} />
+    </Tab.Navigator>
 }
